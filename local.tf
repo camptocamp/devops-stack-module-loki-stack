0166ee8d14af8ba0f2c38482462927e937a7427b
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2022-11-22 11:40:49 +0100

    feat: pass values to HCL structures

--- local.tf
+++ local.tf
@@ -1,3 +1,31 @@
 locals {
-  helm_values = [{}]
+  helm_values = [{
+
+    loki-stack = {
+      loki = {
+        serviceName = "loki-stack.loki-stack"
+        serviceMonitor = {
+          enabled = true
+        }
+      }
+
+      promtail = {
+        serviceMonitor = {
+          enabled = true
+        }
+
+        filebeat = {
+          extraContainers = <<-EOT
+          - name: filebeat-prometheus-exporter
+            image: "trustpilot/beat-exporter:0.4.0"
+            ports:
+            - containerPort: 9479
+              protocol: TCP
+              name: metrics
+        EOT
+        }
+      }
+    }
+  }]
+
 }
