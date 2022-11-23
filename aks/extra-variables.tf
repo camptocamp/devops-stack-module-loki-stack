97b0cf9fe5623b689b0a19103a4bdf9db2dbadd1
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2022-11-23 13:55:43 +0100

    fix: add variable that is not related to the storage

--- aks/extra-variables.tf
+++ aks/extra-variables.tf
@@ -1,3 +1,8 @@
+variable "resource_group_name" {
+  description = "The Resource Group where the Managed Kubernetes Cluster should exist."
+  type        = string
+}
+
 variable "logs_storage" {
   description = "Azure Blob Storage configuration values for the storage container where the logs will be stored."
   type = object({
