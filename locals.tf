29bd9a4d6010c07b9776bf7a054440d16f164897
modridi <mohamed-amine.dridi@camptocamp.com>
2023-01-03 11:42:43 +0100

    feat(Azure): support Loki microservices setup

--- locals.tf
+++ locals.tf
@@ -2,37 +2,128 @@ locals {
   helm_values = [var.distributed_mode ? {
     datasourceURL = "http://${local.fullnameOverride}-query-frontend.${var.namespace}:3100"
     loki-distributed = {
-      fullnameOverride = local.fullnameOverride
+      fullnameOverride = "loki"
+      compactor = {
+        enabled = true
+        persistence = {
+          enabled = true
+        }
+      }
+      gateway = {
+        enabled = false
+      }
+      indexGateway = {
+        enabled = true
+        persistence = {
+          enabled = true
+        }
+      }
+      ingester = {
+        persistence = {
+          enabled = true
+          size = "10Gi"
+        }
+        replicas = 1
+      }
       loki = {
         structuredConfig = {
           chunk_store_config = {
             chunk_cache_config = {
+              enable_fifocache = false
+              memcached = {
+                expiration = "24h"
+              }
               memcached_client = {
                 host = "${local.fullnameOverride}-memcached-chunks.${var.namespace}.svc.cluster.local"
+                service = "http"
+                timeout = "500ms"
               }
             }
           }
-          storage_config = {
-            index_queries_cache_config = {
-              memcached_client = {
-                host = "${local.fullnameOverride}-memcached-index-queries.${var.namespace}.svc.cluster.local"
-              }
+          compactor = {
+            retention_delete_delay = "1h"
+            retention_enabled = false
+          }
+          ingester = {
+            wal = {
+              replay_memory_ceiling = "500MB"
             }
+          }
+          limits_config = {
+            ingestion_rate_mb = 10
+            max_chunks_per_query = 0
+            max_entries_limit_per_query = 0
+            max_query_length = "9000h"
+            max_query_parallelism = 6
+            per_stream_rate_limit = "10MB"
+            retention_period = "9000h"
+          }
+          querier = {
+            max_concurrent = 2
+            query_timeout = "5m"
           }
           query_range = {
+            cache_results = true
+            max_retries = 50
+            parallelise_shardable_queries = false
             results_cache = {
               cache = {
+                enable_fifocache = false
+                memcached = {
+                  expiration = "24h"
+                }
                 memcached_client = {
                   host = "${local.fullnameOverride}-memcached-frontend.${var.namespace}.svc.cluster.local"
+                  service = "http"
+                  timeout = "500ms"
                 }
               }
             }
           }
-          ruler = {
-            # TODO should the next url be set as variable since KPS namespace can change
-            alertmanager_url = "http://kube-prometheus-stack-alertmanager.kube-prometheus-stack:9093"
+          server = {
+            grpc_server_max_recv_msg_size = 33554432
+            grpc_server_max_send_msg_size = 33554432
+            http_server_read_timeout = "180s"
+            http_server_write_timeout = "180s"
+          }
+          storage_config = {
+            boltdb_shipper = {
+              index_gateway_client = {
+                log_gateway_requests = true
+              }
+            }
+            index_queries_cache_config = {
+              enable_fifocache = false
+              memcached = {
+                expiration = "24h"
+              }
+              memcached_client = {
+                host = "${local.fullnameOverride}-memcached-index-queries.${var.namespace}.svc.cluster.local"
+                service = "http"
+                timeout = "500ms"
+              }
+            }
           }
         }
+      }
+      memcachedChunks = {
+        enabled = true
+      }
+      memcachedFrontend = {
+        enabled = true
+      }
+      memcachedIndexQueries = {
+        enabled = true
+      }
+      querier = {
+        affinity = ""
+        replicas = 4
+      }
+      ruler = {
+        directories = {}
+        enabled = false
+        # TODO fix: KPS url is variable
+        alertmanager_url = "http://kube-prometheus-stack-alertmanager.kube-prometheus-stack:9093"
       }
     }
     promtail = {
@@ -68,6 +159,4 @@ locals {
       }
     }
   }]
-
-  fullnameOverride = "loki"
 }
