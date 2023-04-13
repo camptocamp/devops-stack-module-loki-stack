6e7cfa20f64f4781c7d44fedf6d7e2d09078e90d
Mohamed Amine Dridi <78367821+modridi@users.noreply.github.com>
2023-04-13 18:34:15 +0200

    feat(azure)!: use managed identity to access object storage (#40)
    * feat(azure)!: use managed identity to access object storage
    
    * docs(terraform-docs): generate docs and write to README.adoc
    
    ---------
    
    Co-authored-by: modridi <modridi@users.noreply.github.com>

--- aks/locals.tf
+++ aks/locals.tf
@@ -1,7 +1,24 @@
 locals {
+  use_managed_identity = var.logs_storage.managed_identity_node_rg_name != null
+
   helm_values = [var.distributed_mode ? {
-    loki-distributed = {
-      loki = {
+    loki-distributed = merge(local.use_managed_identity ? {
+      serviceAccount = {
+        annotations = {
+          "azure.workload.identity/client-id" = azurerm_user_assigned_identity.loki[0].client_id
+        }
+      }
+      } : null, {
+      loki = merge(local.use_managed_identity ? {
+        # version >= 2.8 is required for workload identity support. Current chart version uses loki 2.7.5.
+        # TODO remove once chart uses a version >= 2.8.
+        image = {
+          tag = "2.8.0"
+        }
+        podLabels = {
+          "azure.workload.identity/use" = "true"
+        }
+        } : null, {
         schemaConfig  = local.schema_config
         storageConfig = local.storage_config
         structuredConfig = {
@@ -10,18 +27,33 @@ locals {
             working_directory = "/var/loki"
           }
         }
-      }
-    }
+      })
+    })
     } : null, var.distributed_mode ? null : {
     loki-stack = {
-      loki = {
+      loki = merge(local.use_managed_identity ? {
+        # Current chart version uses loki 2.6.1.
+        # TODO remove once chart uses a version >= 2.8.
+        image = {
+          tag = "2.8.0"
+        }
+        serviceAccount = {
+          annotations = {
+            "azure.workload.identity/client-id" = azurerm_user_assigned_identity.loki[0].client_id
+          }
+        }
+        podLabels = {
+          "azure.workload.identity/use" = "true"
+        }
+        } : null, {
         config = {
           schema_config  = local.schema_config
           storage_config = local.storage_config
         }
-      }
+      })
     }
-  }]
+    }
+  ]
 
   schema_config = {
     configs = [
@@ -39,15 +71,17 @@ locals {
   }
 
   storage_config = {
-    azure = {
-      container_name  = var.logs_storage.container
-      account_name    = var.logs_storage.storage_account
-      account_key     = var.logs_storage.storage_account_key
-      request_timeout = "180s"
-      max_retries     = 50
-      min_retry_delay = "1s"
-      max_retry_delay = "5s"
-    }
+    azure = merge(local.use_managed_identity ? null : {
+      account_key = var.logs_storage.storage_account_key
+      }, {
+      use_federated_token = local.use_managed_identity
+      container_name      = var.logs_storage.container
+      account_name        = var.logs_storage.storage_account
+      request_timeout     = "180s"
+      max_retries         = 50
+      min_retry_delay     = "1s"
+      max_retry_delay     = "5s"
+    })
     boltdb_shipper = {
       shared_store = "azure"
     }
