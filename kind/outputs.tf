35d92f4ae9d69897ef41057ac411b71ad988097c
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-06-05 12:00:38 +0200

    docs: add descriptions to outputs

--- kind/outputs.tf
+++ kind/outputs.tf
@@ -1,8 +1,10 @@
 output "id" {
-  value = module.loki-stack.id
+  description = "ID to pass other modules in order to refer to this module as a dependency."
+  value       = module.loki-stack.id
 }
 
 output "loki_credentials" {
-  value     = module.loki-stack.loki_credentials
-  sensitive = true
+  description = "Credentials to access the Loki ingress, if activated."
+  value       = module.loki-stack.loki_credentials
+  sensitive   = true
 }
